97abca112ef7685ea67b8fb0ad86a8d5e0fc2894
Ceronath <jordan_poulin10@hotmail.com>
2020-12-06 12:48:28 -0500

    done lab 5

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.geoquiz;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,34 +16,47 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "MainActivity";
+    private final String KEY_INDEX = "index";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
-    private Question[] mQuestionBank = {
-            new Question(R.string.question_australia, true),
-            new Question(R.string.question_oceans, true),
-            new Question(R.string.question_mideast, false),
-            new Question(R.string.question_africa, false),
-            new Question(R.string.question_americas, true),
-            new Question(R.string.question_asia, true)
+    private Button mCheatButton;
+    private final int REQUEST_CODE_CHEAT = 0;
 
-    };
-    private int mCurrentIndex = 0;
-
-    public MainActivity() {
-    }
-
+    public MainActivity(){
+        }
+    private QuizViewModel mQuizViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
 
+        ViewModelProvider provider = ViewModelProviders.of(this);
+        mQuizViewModel = provider.get(QuizViewModel.class);
+        Log.d(TAG, "Got a QuizViewModel: $quizViewModel");
+
+        if(savedInstanceState != null){
+            mQuizViewModel.mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        } else {
+            mQuizViewModel.mCurrentIndex = 0;
+        }
+
         mTrueButton = (Button) findViewById(R.id.trueButton);
         mFalseButton = (Button) findViewById(R.id.falseButton);
         mNextButton = findViewById(R.id.nextButton);
+        mCheatButton = findViewById(R.id.cheatButton);
         mQuestionTextView = findViewById(R.id.question_text_view);
+
+        mCheatButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                boolean answerIsTrue = mQuizViewModel.currentQuestionAnswer();
+                Intent intent = CheatActivity.newIntent(MainActivity.this, answerIsTrue);
+                startActivityForResult(intent, REQUEST_CODE_CHEAT);
+            }
+        });
 
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -57,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         mNextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mQuizViewModel.moveToNext();
                 updateQuestion();
             }
         });
@@ -65,6 +83,28 @@ public class MainActivity extends AppCompatActivity {
         updateQuestion();
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode != Activity.RESULT_OK){
+            return;
+        }
+
+    if(requestCode == REQUEST_CODE_CHEAT){
+        if(data != null){
+            mQuizViewModel.setIsCheater(data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false));
+        }
+    }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mQuizViewModel.mCurrentIndex);
     }
 
     @Override
@@ -97,14 +137,15 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onDestroy() called.");
     }
     private void updateQuestion(){
-        int questionTextResId = mQuestionBank[mCurrentIndex].getTextResId();
+        int questionTextResId = mQuizViewModel.currentQuestionText();
         mQuestionTextView.setText(questionTextResId);
     }
 
     private void checkAnswer(boolean answer){
-        boolean correctAnswer = mQuestionBank[mCurrentIndex].isAnswer();
-
-        if (answer == correctAnswer){
+        boolean correctAnswer = mQuizViewModel.currentQuestionAnswer();
+        if(mQuizViewModel.isCheater()){
+            Toast.makeText(this, R.string.judgment_toast, Toast.LENGTH_SHORT).show();
+        } else if (answer == correctAnswer){
             Toast.makeText(this, R.string.toastCorrect,Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, R.string.toastWrong, Toast.LENGTH_SHORT).show();
